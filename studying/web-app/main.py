2afc1df07993410a6948d8f645f91d7b807af8e1
Andrew <isn't-email@exmp.com>
2019-10-25 17:49:46 +0200

    Work with cookies

--- studying/web-app/main.py
+++ studying/web-app/main.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, make_response, abort
 
 app = Flask(__name__)
 
-data = {"var1": "val1", "var2": "val2"}
+data = {"var1": "val1", "var2": "val2", "var3": "val3"}
 
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def result():
+    return render_template('home.html')
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+
+@app.route("/setcookie", methods = ["POST"])
+def success():
+    if request.method == "POST":
+        user = request.form['username']
+        surname = request.form['usersurname']
+        resp = make_response(render_template('readcookie.html'))
+        resp.set_cookie('IDuser', user, surname)
+
+        return resp
+
+
+@app.route("/getcookie")
+def getcookie():
+    return request.cookies.get('IDuser')
 
 
 if __name__ == "__main__":
