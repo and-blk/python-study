179c78a6ea466e8e4f84e8fd9593bad1892fccc6
Andrew <isn't-email@exmp.com>
2019-11-14 11:43:56 +0100

    code with form and part of DB

---
+++ microblog/webapp/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from webapp.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+login = LoginManager(app)
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+from webapp import routes, models
+
---
+++ microblog/webapp/routes.py
@@ -0,0 +1,28 @@
+from flask import render_template, redirect, url_for, flash
+from webapp import app
+from webapp.form import LoginForm
+
+
+SQLALCHEMY_DATABASE_URI = app.config['SQLALCHEMY_DATABASE_URI']
+SQLALCHEMY_TRACK_MODIFICATIONS = app.config['SQLALCHEMY_TRACK_MODIFICATIONS']
+
+
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template("index.html", title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title='about')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('reqested access for {}'.format(form.username.data))
+        return redirect(url_for("home"))
+    return render_template('login.html', form=form, title='login')
