80eff34242393cb370fa53ef984fbf5ab5c2e600
Andrew <isn't-email@exmp.com>
2019-11-15 07:45:24 +0100

    add htmls correct collection kernels commit whole project

--- microblog/microblog_start.py
+++ microblog/microblog_start.py
@@ -1,7 +1,8 @@
-from webapp import app, db
+from webapp import app, db, GetVersion
 from webapp.models import User, Post
+
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'app': app}
+    return {'db': db, 'User': User, 'Post': Post, 'app': app, 'GetVersion': GetVersion}
