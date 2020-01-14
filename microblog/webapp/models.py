ee0c2a87ccae5b13ebec429fef5bd771ce970598
Andrey_L <test@my_mail.com>
2020-01-14 16:27:38 +0100

    modified:   models.py some comments

--- microblog/webapp/models.py
+++ microblog/webapp/models.py
@@ -2,7 +2,7 @@ from webapp import db
 from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 
-#Hey there this is my file
+#Hey there this is my 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True)
